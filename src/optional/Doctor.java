8a765ae950315a020409340994a4669c3ced0bed
Barezi79 <branislavnesic79@gmail.com>
2019-06-13 11:58:19 +0200

    Exercise Doctor

---
+++ src/optional/Doctor.java
@@ -0,0 +1,20 @@
+package optional;
+
+public class Doctor {
+	
+	private String name;
+	
+	public Doctor (String name) {
+		this.name=name;
+	}
+
+	public String getName() {
+		return "My name is " + name + ".";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+		
+
+}
