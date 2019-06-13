0ec914e9be11b3e5ac5c4b086421734774b9c3be
Barezi79 <branislavnesic79@gmail.com>
2019-06-13 19:40:21 +0200

    "Surgeon"

---
+++ src/optional/DoctorMain.java
@@ -0,0 +1,20 @@
+package optional;
+
+public class DoctorMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GeneralPractitioner gp = new GeneralPractitioner("Dr.Maria");
+		System.out.println(gp.getName());
+		gp.doHouseVisits();
+		gp.doMedicine();
+		
+		Surgeon s = new Surgeon("Dr.Mike");
+		System.out.println(s.getName());
+		s.performSurgery();
+		s.doConsultation();
+
+	}
+
+}
