cbe476de31aea89298b0a7093a4eb23c00fb9444
Barezi79 <branislavnesic79@gmail.com>
2019-06-15 16:05:00 +0200

    Surgeon

---
+++ src/nesic/branislav/Surgeon.java
@@ -0,0 +1,19 @@
+package nesic.branislav;
+
+public class Surgeon extends Doctor {
+
+	public Surgeon(String name) {
+		super(name);
+	}
+
+	void performSurgery() {
+		System.out.println("I am a Surgery doctor. I operate on patients to treat their diseases or injuries.");
+	}
+
+	void doConsultation() {
+		System.out.println("Usually I do consulatations with other colleagues during the surgery and after"
+				+ " to give the best treatment to our patients.");
+		
+	}
+
+}
